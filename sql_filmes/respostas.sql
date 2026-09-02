165bb4f8288b682f953a8fd7f39b1eaf361122b0
Jota <jota4wd@gmail.com>
2026-09-01 22:02:55 -0300

    corrigindo enunciado sql

--- sql_filmes/respostas.sql
+++ sql_filmes/respostas.sql
@@ -6,8 +6,8 @@
 -- 1 - Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM Filmes;
 
--- 2 - Buscar o nome, ano e duração dos filmes
-SELECT Nome, Ano, Duracao FROM Filmes;
+-- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano ASC;
 
 -- 3 - Buscar pelo filme De Volta para o Futuro, trazendo o nome, ano e a duração
 SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro';
